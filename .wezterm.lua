838ade2d8148478ec3adb1769b9b25c904261c00
SaadShah11 <shahsaadullah11@gmail.com>
2025-07-30 19:38:26 +0500

    first commit

---
+++ .wezterm.lua
@@ -0,0 +1,35 @@
+-- Pull in the wezterm API
+local wezterm = require 'wezterm'
+
+-- This will hold the configuration.
+local config = wezterm.config_builder()
+
+-- This is where you actually apply your config choices.
+
+-- For example, changing the initial geometry for new windows:
+config.initial_cols = 120
+config.initial_rows = 28
+
+-- or, changing the font size and color scheme.
+config.font_size = 12
+config.color_scheme = 'Sakura'
+
+config.window_background_image = 'C:/Users/NESL-10043/Downloads/hollow knight 2.jpg'
+
+config.window_background_image_hsb = {
+  -- Darken the background image by reducing it to 1/3rd
+  brightness = 0.3,
+
+  -- You can adjust the hue by scaling its value.
+  -- a multiplier of 1.0 leaves the value unchanged.
+  hue = 1.0,
+
+  -- You can adjust the saturation also.
+  saturation = 1.0,
+}
+
+config.window_background_opacity = 0.9
+
+-- Finally, return the configuration to wezterm:
+return config 
+
